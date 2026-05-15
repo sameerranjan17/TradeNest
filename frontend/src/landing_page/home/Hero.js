16df25b4afcd7bc4473c8593315d1aa8b4485513
sameerranjan17 <sameerranjan7794@gmail.com>
2026-05-15 22:05:46 +0530

    Update Hero and Footer links

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -5,6 +5,7 @@ function Hero() {
     <div className="container p-5 mb-5">
       <div className="row text-center">
         <img src="media/images/homeHero.png" alt="Hero Image" className="mb-5" />
+        {/* <img src="media/images/homeHeroTradeNest.png" alt="Hero Image" className="mb-5" /> */}
         <h1 className="mt-5">Invest in everything</h1>
         <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
         <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin: "0 auto"}}>Signup Now</button>
